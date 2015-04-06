e2a3844e10a1c0552ecda480a6039bbcb10bb7b2
Notika314 <notika314@gmail.com>
2015-04-06 12:19:28 -0400

    modify models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,4 @@
 class Item < ActiveRecord::Base
   belongs_to :user
   has_many :comments
-  attr_accessible :
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :comments
   has_many :items
-  attr_accessible :name, :email, :password
 
    def may_edit?(item)
     item.user_id == self.id
